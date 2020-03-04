1594414be60395603778e0fdfddb15e6208c8664
M Shanmugavel <shanmugavel.m@sella.it>
2020-03-04 19:25:58 +0530

    Microservices configuration server samples

---
+++ cloudserver/src/main/java/com/sample/mscs/CloudserverApplication.java
@@ -0,0 +1,17 @@
+package com.sample.mscs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CloudserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudserverApplication.class, args);
+	}
+
+}
+
+
